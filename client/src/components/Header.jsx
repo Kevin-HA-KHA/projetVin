ecd0217a0627ae98cba678822004cdf697e6e706
Kevin-HA-KHA <nivekah27@gmail.com>
2024-05-24 16:57:05 +0200

    first commit kevin

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { errorReset } from '../redux/user/userSlice';
 import "../styles/Header.css"
+import logo from "../assets/images/logo.png";
 
 export default function Header() {
   const dispatch = useDispatch();
@@ -12,22 +13,31 @@ export default function Header() {
   return (
     <div className='header'>
       <div className='nav'>
-        <ul className='nav-droite'>
-          <Link to='/'>
-            { currentUser ? <li>Accueil</li> : null}
-          </Link>
-          <Link to='/profile_page'>
-            { currentUser ? <li>Profil</li> : null}
-          </Link>
-          <Link to='/about'>
-            { currentUser ? <li>Qui sommes-nous ?</li> : null}
-          </Link>
-          <Link to='/produits'>
-            { currentUser ? <li>Nos produits</li> : null}
-          </Link>
-          <Link to='/rencontrer'>
-            { currentUser ? <li>Nous rencontrer</li> : null}
-          </Link>
+        <ul className='nav-box'>
+          <div className="nav-left">
+            <Link to='/'>
+              <img className='logo' src={logo} alt="logo" />
+              {/* { currentUser ? <li>Accueil</li> : null} */}
+            </Link>
+          </div>
+          <div className='nav-right'>
+            <Link to='/about'>
+              <li>Qui sommes-nous</li>
+            </Link>
+            <Link to='/produits'>
+              <li>Nos produits</li>
+            </Link>
+            <Link to='/rencontrer'>
+              <li>Nous rencontrer</li>
+            </Link>
+            { currentUser ?
+              <Link to='/profile_page'>
+                { currentUser ? <li>Profil</li> : null}
+              </Link>
+              :
+              null
+            }
+          </div>
           {/* <Link to='/aventure'>
             { currentUser ? <li>Aventure</li> : null}
           </Link>
