89d7709bae8f6398165197d73d231168c16fe320
Kevin-HA-KHA <nivekah27@gmail.com>
2024-08-23 23:15:58 +0000

    fix gestion admin by correcting mistake on request's url

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://projetvin.onrender.com',
         secure: false,
       },
     },
