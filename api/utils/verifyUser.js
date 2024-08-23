9748d6fa3729553f6c4f476e9bfa0859493a9da7
Kevin-HA-KHA <nivekah27@gmail.com>
2024-08-23 22:20:29 +0000

    fix gestion admin by changing cors origin to actual domain and not *

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 import { errorHandler } from './error.js';
 
 export const verifyToken = (req, res, next) => {
-    const token = req.cookies.access_token;
+    const {token} = req.cookies;
 
     if (!token) return next(errorHandler(401, 'You are not authenticated!'));
 
